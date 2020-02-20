969695f7d35f5088ede4c88a1fb16a9770fea0ee
Tiago Koubo <tiagokoubo@gmail.com>
2020-02-20 15:01:44 -0300

    Ajuste

--- pythonT.py
+++ pythonT.py
@@ -1,19 +1,31 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-driver = webdriver.Firefox(executable_path=r'/Users/alexandreoliveira/Downloads/geckodriver')
+driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.get('http://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/historico/derivativos/ajustes-do-pregao/')
 
+time.sleep(5)
+
 def consultar():
-    class_data = 'body > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > div:nth-child(1) > form:nth-child(7) > div:nth-child(2) > fieldset:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > a:nth-child(3)'
-    class_consulta = '/html/body/div[1]/div[1]/div/form/div/div[2]'
+   # class_consulta = '/html/body/div[1]/div[1]/div/form/div/div[2]'
+    driver.switch_to.frame("bvmf_iframe")
+    data_today = '14/02/2020'
+    time.sleep(4)
+    element = driver.find_element_by_name('dData1')
+    element.clear()
+    element.send_keys(data_today)
+    driver.find_element_by_xpath('//*[@id="divContainerIframeBmf"]/div[1]/div/form/div/div[2]/button').click()
 
-    data_today = '15012020'
+    # element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.NAME, 'btnK')))
+    # driver.execute_script("arguments[0].click();", element)
+    #btn_search = driver.find_element_by_xpath(class_consulta)
 
-    input_data = driver.find_element_by_css_selector(class_data)
-    btn_search = driver.find_element_by_xpath(class_consulta)
-
-    input_data.send_keys(data_today)
-    btn_search.click()
+#    input_data.send_keys(data_today)
+ #   btn_search.click()
 
 consultar()
